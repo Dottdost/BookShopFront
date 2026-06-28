a3200cc5686dbc950b9872404d57c01133b4fb0f
Gama <elikosamed@gmail.com>
2026-06-28 15:11:44 +0400

    Fix orders background refresh

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -1,35 +1,51 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../store";
-import { Order } from "../types/order";
+import { DispatchType, RootState } from "../store";
 import { addOrder, fetchOrders } from "../store/slices/ordersSlice";
-import { useEffect } from "react";
-import { ThunkDispatch } from "@reduxjs/toolkit";
-import { Action } from "redux";
-
-type DispatchType = ThunkDispatch<RootState, undefined, Action>;
+import { Order } from "../types/order";
 
 export const useOrders = (userId: string) => {
   const dispatch: DispatchType = useDispatch();
 
   const orders = useSelector((state: RootState) => state.orders.items);
-  const loading = useSelector((state: RootState) => state.orders.loading);
+  const reduxLoading = useSelector((state: RootState) => state.orders.loading);
   const error = useSelector((state: RootState) => state.orders.error);
+
+  const [firstLoadDone, setFirstLoadDone] = useState(false);
 
   useEffect(() => {
     if (!userId) return;
 
-    dispatch(fetchOrders(userId));
+    let cancelled = false;
+
+    const loadFirstTime = async () => {
+      await dispatch(fetchOrders(userId));
+
+      if (!cancelled) {
+        setFirstLoadDone(true);
+      }
+    };
+
+    void loadFirstTime();
 
     const interval = window.setInterval(() => {
-      dispatch(fetchOrders(userId));
+      void dispatch(fetchOrders(userId));
     }, 2000);
 
-    return () => window.clearInterval(interval);
+    return () => {
+      cancelled = true;
+      window.clearInterval(interval);
+    };
   }, [userId, dispatch]);
 
   const placeOrder = (order: Order) => {
     dispatch(addOrder(order));
   };
 
-  return { orders, placeOrder, loading, error };
+  return {
+    orders,
+    placeOrder,
+    loading: reduxLoading && !firstLoadDone,
+    error,
+  };
 };
